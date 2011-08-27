2674577a6f147298f777ce740808a7cf625d6736
Daniel Friesel <derf@finalrewind.org>
2011-08-27 09:20:39 +0200

    Minor code fixes
    * seed random number generator with time [seconds] + PID instead of just
      time [nanoseconds]
    * Improve usage message

--- src/randsleep.c
+++ src/randsleep.c
@@ -5,19 +5,15 @@
 
 int main (int argc, char **argv) {
 	long maxsleep;
-	struct timespec ntime;
 
 	if (argc < 2) {
-		fputs("Usage: randsleep <max> [<cmd>]\n", stdout);
+		fputs("Usage: randsleep <max> [<cmd> [<args ...>]]\n", stderr);
 		return 1;
 	}
 
 	maxsleep = atol(argv[1]);
 
-	if (clock_gettime(CLOCK_REALTIME, &ntime) == -1)
-		perror("clock_gettime");
-
-	srand(ntime.tv_nsec);
+	srand(time(NULL) + getpid());
 
 	sleep((rand() % maxsleep) + 1);
 
